4c76a456f718c32dc29b11e3d9b91f2cf06e5aad
Shivansh Raghav <shivanshraghav7@gmai.com>
2023-07-15 00:30:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,34 @@
 //complete this code
-class Person {}
+class Person {
+	constrcutor(name, age){
+		this.name=name
+		this.age=age
+	}
+	getter(){
+		return this.name
+	}
+	setter(number){
+		this.age=number
+	}
+}
 
-class Student extends Person {}
+class Student extends Person {
+	constructor(name,age){
+		Person.call(this,name,age)
+	}
+	study(){
+		console.log(`${this.name} is studying`)
+	}
+}
 
-class Teacher extends Person {}
+class Teacher extends Person {
+	constructor(name,age){
+		Person.call(this,name,age)
+	}
+	teach(){
+		console.log(`${this.name} is teaching`)
+	}
+}
 
 // Do not change the code below this line
 window.Person = Person;
