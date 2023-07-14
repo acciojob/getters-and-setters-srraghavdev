714e05142bc9c3cee727dec661004a3515973db7
Shivansh Raghav <shivanshraghav7@gmai.com>
2023-07-15 00:45:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,32 +1,31 @@
 class Person {
-	constructor(name,age){
-		this.name=name
-		this.age=age
-	}
-	getter(){
-		return this.name
-	}
-	setter(number){
-		this.age=number
-	}
+  constructor(name, age) {
+    this._name = name;
+    this._age = age;
+  }
+ 
+  get name() {
+    return this._name;
+  }
+ 
+  /**
+   * @param {any} age
+   */
+  set age(age) {
+    this._age = age;
+  }
 }
-
+ 
 class Student extends Person {
-	constructor(name,age){
-		super(name,age)
-	}
-	study(){
-		console.log(`${this.name} is studying`)
-	}
+  study() {
+    console.log(`${this.name} is studying`);
+  }
 }
-
+ 
 class Teacher extends Person {
-	constructor(name,age){
-		super(name,age)
-	}
-	teach(){
-		console.log(`${this.name} is teaching`)
-	}
+  teach() {
+    console.log(`${this.name} is teaching`);
+  }
 }
 // Do not change the code below this line
 window.Person = Person;
